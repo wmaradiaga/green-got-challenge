e6bbd19da8abf4e272c99e8ea55b1218a94892e5
Williams Maradiaga <wifran10@gmail.om>
2021-02-07 19:49:31 -0600

    chore: fallback to all methods except POST

--- pages/api/create_user.tsx
+++ pages/api/create_user.tsx
@@ -5,10 +5,7 @@ import { check, validationResult } from "express-validator";
 
 const validateBody = initMiddleware(
   validateMiddleware(
-    [
-      check("firstName").isString().isLength({ min: 1 }),
-      check("lastName").isString().isLength({ min: 1 }),
-    ],
+    [check("firstName").notEmpty(), check("lastName").notEmpty()],
     validationResult
   )
 );
@@ -21,11 +18,12 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       const errors = validationResult(req);
       if (!errors.isEmpty())
         return res.status(422).json({ errors: errors.array() });
-        
+
       const { firstName, lastName } = req.body;
-      res.status(200).send({
+      return res.status(200).send({
         message: `${firstName.toUpperCase()} ${lastName.toUpperCase()}`,
       });
-      return;
+    default:
+      return res.status(401).send({ message: "Not implemented yet" });
   }
 };
